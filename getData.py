6588f8668023ab606251f1c37a8e1c80a5eadee9
Kristian Urup Olesen Larsen <qsd161@SAMF.DOMAIN>
2017-06-14 11:21:22 +0200

    drop class

--- getData.py
+++ getData.py
@@ -2,71 +2,70 @@ import requests
 import csv
 import os
 
-class dst():
 
-    def table(id, vars = False, values = False):
+def table(id, vars = False, values = False):
         # TOdo: tolower alt
 
-        if not vars:
-            print('''
-            no variables selected - choose variables with dst.metadata()
-            Setting vars to API default
-            ''')
-            vars = ''
+    if not vars:
+        print('''
+        no variables selected - choose variables with dst.metadata()
+        Setting vars to API default
+        ''')
+        vars = ''
 
-        if not values:
-            print('''
-            No values selected - setting values to API default
-            ''')
-            values = {}
-            for i in vars:
-                values[i] = ['*']
-
-        base = 'http://api.statbank.dk/v1/data/'
-        form = '/CSV?lang=en'
-
-        baseLink = base + id + form
-
+    if not values:
+        print('''
+        No values selected - setting values to API default
+        ''')
+        values = {}
         for i in vars:
-            baseLink = baseLink + "&" + i + "="
-            for j in values[i]:
-                 baseLink = baseLink + j + ','
-            baseLink = baseLink[:-1]
-        print(baseLink)
-        return requests.get(baseLink)
+            values[i] = ['*']
+
+    base = 'http://api.statbank.dk/v1/data/'
+    form = '/CSV?lang=en'
+
+    baseLink = base + id + form
+
+    for i in vars:
+        baseLink = baseLink + "&" + i + "="
+        for j in values[i]:
+             baseLink = baseLink + j + ','
+        baseLink = baseLink[:-1]
+    print(baseLink)
+    return requests.get(baseLink)
 
 
-    def subject(id):
-        base = 'http://api.statbank.dk/v1/subjects/'
-        form = '?lang=en&format=JSON'
+def subject(id):
+    base = 'http://api.statbank.dk/v1/subjects/'
+    form = '?lang=en&format=JSON'
 
-        link = base + id + form
-        respJSON = requests.get(link).json()
+    link = base + id + form
+    respJSON = requests.get(link).json()
 
-        return respJSON
+    return respJSON
 
-    def metadata(id):
-        base = 'http://api.statbank.dk/v1/tableinfo/'
-        form = '?lang=en&format=JSON'
+def metadata(id):
+    base = 'http://api.statbank.dk/v1/tableinfo/'
+    form = '?lang=en&format=JSON'
 
-        link = base + id + form
-        respJSON = requests.get(link).json()['variables']
-        print("There are ", len(respJSON), "variables in ", id, "- acces them with [n]")
+    link = base + id + form
+    respJSON = requests.get(link).json()['variables']
+    print("There are ", len(respJSON), "variables in ", id, "- acces them with [n]")
 
-        return respJSON
+    return respJSON
 
 
-    def toCSV(table, name):
-        table = table.text.replace(",","-")
+def toCSV(table, name):
+    table = table.text.replace(",","-")
 
-        fileName = name +".csv"
-        if not os.path.exists(fileName):
-            open(fileName,'w+').close()
+    fileName = name +".csv"
+    if not os.path.exists(fileName):
+        open(fileName,'w+').close()
 
-        with open(fileName,'w') as curCSV:
-            writer = csv.writer(curCSV, delimiter = ',', lineterminator = "\n")
-            for row in table[1:].split("\r\n"):
-                writer.writerow([row])
+    with open(fileName,'w') as curCSV:
+        writer = csv.writer(curCSV, delimiter = ',', lineterminator = "\n")
+        for row in table[1:].split("\r\n"):
+            writer.writerow([row])
 
 
 getList = [["NAN1", ["TRANSAKT","PRISENHED","Tid"]],
