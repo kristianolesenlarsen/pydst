f5f74f2c499d57136b1241b7a21b9c4c1c7798e1
Kristian Urup Olesen Larsen <qsd161@SAMF.DOMAIN>
2017-06-25 20:17:06 +0200

    figures

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 apiDST holds functions to do basic interaction with the API of Statistics Denmark. Beyond that the idea of this project is to build a self-updating page with graphs of key figures of the Danish economy and society.  
 
-
 ![GDP](gdp.png)
--- figures.py
+++ figures.py
@@ -7,7 +7,7 @@ import matplotlib as mpl
 nan1 = pd.read_csv("NAN1.csv", sep = ";", na_values = "..", dtype = {'TRANSAKT': str, 'PRISENHED': str,  'INDHOLD': float}, parse_dates = ['TID'])
 aku100 = pd.read_csv("AKU100.csv", sep = ";", na_values = "..", dtype = {'BESKSTATUS': str, 'ALDER': str, 'INDHOLD': float},parse_dates = ['TID'])
 pris112 = pd.read_csv("PRIS112.csv", sep = ";", na_values = "..", dtype = {'HOVED': str, 'ALDER': str, 'INDHOLD': float},parse_dates = ['TID'])
-
+folk2 = pd.read_csv("FOLK2.csv", sep = ";", na_values = "..", dtype = {'HERKOMST': str, 'INDHOLD': float},parse_dates = ['TID'] )
 
 replacements_NAN1_TRANS = {
 'TRANSAKT': {'B.1*g Gross domestic product': 'GDP',
@@ -22,7 +22,7 @@ nan1 = nan1.replace(replacements_NAN1_TRANS).replace(replacements_NAN1_PRIS)
 #drop unnessecary rows in column TRANSAKT
 
 '''
-PLOT GDP
+PLOT GDP (fix the subsetting)
 '''
 dat_gdp = nan1[nan1.TRANSAKT == 'GDP']
 dat_gdp = dat_gdp[(dat_gdp.PRISENHED == 'chainedPrices') | (dat_gdp.PRISENHED == 'currentPrices')]
@@ -73,3 +73,12 @@ for i in aku100.BESKSTATUS.unique():
         plt.plot(grp['TID'], grp['INDHOLD'], label = key)
         plt.legend(loc = 'best')
     plt.show()
+
+
+'''
+PLOT FOLK2
+'''
+for key,grp in folk2.groupby(['HERKOMST']):
+    plt.plot(grp['TID'], grp['INDHOLD'], label = key)
+    plt.legend(loc = 'best')
+plt.show()
