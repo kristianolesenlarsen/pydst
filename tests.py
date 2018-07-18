2c6ebc955fb35f5bfc57116cff6c1458f7dd069f
Kristian <kristianuruplarsen@gmail.com>
2018-07-18 14:09:06 +0200

    first attempt at testing code

---
+++ tests.py
@@ -0,0 +1,12 @@
+import os
+import pkg_resources
+DATA_PATH = pkg_resources.resource_filename('PyDST', 'data/')
+
+
+def are_files_installed():
+    assert set(os.listdir(DATA_PATH)) == set(['KOMMUNE.dbf', 'KOMMUNE.prj', 'KOMMUNE.shp', 'KOMMUNE.shx',
+                                       'SOGN.dbf', 'SOGN.prj', 'SOGN.shp', 'SOGN.shx'])
+
+
+if __name__ == '__main__':
+    are_files_installed()
