078caf070744dbee2999ee043b861e1ce778a096
Kristian <kristianuruplarsen@gmail.com>
2018-07-18 14:15:24 +0200

    tests

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ DATA_PATH = pkg_resources.resource_filename('PyDST', 'data/')
 
 
 def are_files_installed():
+    print("If this test passes, data are installed correctly")
     assert set(os.listdir(DATA_PATH)) == set(['KOMMUNE.dbf', 'KOMMUNE.prj', 'KOMMUNE.shp', 'KOMMUNE.shx',
                                        'SOGN.dbf', 'SOGN.prj', 'SOGN.shp', 'SOGN.shx'])
 
