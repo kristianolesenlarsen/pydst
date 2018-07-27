0aa96ff7d25529ecf99a7e7119f141683cfb299d
Kristian <kristianuruplarsen@gmail.com>
2018-07-27 19:40:02 +0200

    remove data from setup

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup
 
 setup(name='PyDST',
       version='0.1',
-      include_package_data = True,
-      package_data = {'PyDST': 'data/*.*'},
       description='Grab data from Statistics Denmark',
       url='http://github.com/KristianUrupLarsen/PyDST',
       author='Kristian Urup Olesen Larsen',
@@ -15,5 +13,3 @@ setup(name='PyDST',
         'requests'
       ],
 zip_safe=False)
-
-# Also geopandas is a requirement but you need to get this through conda forge
