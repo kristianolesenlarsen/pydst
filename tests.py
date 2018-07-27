781846d962f4f50c0fdd0b21bd55b9fc0c449630
Kristian <kristianuruplarsen@gmail.com>
2018-07-27 17:38:53 +0200

    geopandas

--- tests.py
+++ tests.py
@@ -10,16 +10,16 @@ def are_files_installed():
 
 def geopandas():
     import geopandas as gpd
-    try:
-        print('loaded files as gpd object...')
-        x = gpd.read_file(DATA_PATH + '/KOMMUNE.shp')
-        print('plotted map...')
-        x.plot()
-        x = True
-    except:
-        x = None
 
-    assert x is not None
+    try:
+        x = gpd.read_file(DATA_PATH + '/KOMMUNE.shp')
+        print('loaded files as gpd object...')
+        x.plot()
+        print('plotted map...')
+
+    except:
+        raise AssertionError('Cannot use geopandas.')
+
     print('Geopandas is working properly.')
 
 
