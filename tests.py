e97156e400e178209b7085c6227c7308e19f2199
Kristian <kristianuruplarsen@gmail.com>
2018-07-27 17:29:35 +0200

    tests

--- tests.py
+++ tests.py
@@ -4,20 +4,24 @@ DATA_PATH = pkg_resources.resource_filename('PyDST', 'data/')
 
 
 def are_files_installed():
-    print("If this test passes, data are installed correctly")
-    assert set(os.listdir(DATA_PATH)) == set(['KOMMUNE.dbf', 'KOMMUNE.prj', 'KOMMUNE.shp', 'KOMMUNE.shx',
+        assert set(os.listdir(DATA_PATH)) == set(['KOMMUNE.dbf', 'KOMMUNE.prj', 'KOMMUNE.shp', 'KOMMUNE.shx',
                                        'SOGN.dbf', 'SOGN.prj', 'SOGN.shp', 'SOGN.shx'])
+        print('Files are properly installed.')
 
 def geopandas():
     import geopandas as gpd
     try:
         x = gpd.read_file(DATA_PATH + '/KOMMUNE.shp')
+        x.plot()
     except:
         x = None
 
     assert x is not None
+    print('Geopandas is working properly.')
 
 
 if __name__ == '__main__':
+    print('assessing that all geo-files are installed.')
     are_files_installed()
+    print('Checking if geopandas works')
     geopandas()
